6f1f11b8245e4b76e37216fb269116c1d0c2c3c0
Mojtaba <mojtabamomenaei@gmail.com>
2025-04-20 18:30:35 +0330

    hard code a simple example

--- main.py
+++ main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
+nt1 = 'A'
+rule1, rule2 = "aA", 'aB'
+nt2 = 'B'
+rule3, rule4 = 'bB', 'c'
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+lookahead = 0
+keyw = 'ac'
 
 
-# Press the green button in the gutter to run the script.
+
+def error():
+    print('bib bib, you lose. Could not generate the given string with this grammar.')
+    exit()
+def match(in1):
+    global lookahead
+    if keyw[lookahead] == in1:
+        lookahead += 1
+    else:
+        error()
+
+def A(in1):
+    global lookahead
+    try:
+        if lookahead == len(in1):
+            error()
+        if in1[lookahead] in 'a':
+            match('a')
+            if lookahead == len(in1):
+                error()
+            if in1[lookahead] in 'a':
+                A(in1)
+                return
+            elif in1[lookahead] in 'bc':
+                B(in1)
+                return
+            else: error()
+        else:
+            error()
+    except():
+        error()
+
+
+def B(in1):
+    try:
+        global lookahead
+        if lookahead == len(in1):
+            error()
+        if in1[lookahead] in 'b':
+            match('b')
+            B(in1)
+            return
+        elif in1[lookahead] in 'c':
+            match('c')
+            if lookahead != len(in1):
+                error()
+            print('Mission accomplished, soldier! The given string can be perfectly generated using this grammar.')
+
+            exit()
+        else:
+            error()
+    except():
+        error()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    A(keyw)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
+
+
+
